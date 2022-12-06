dd88ba3ce29543d7be4a846bd05a24177a90d5e2
Daria Rabiichuk <115396338+darr4i@users.noreply.github.com>
2022-12-06 13:38:27 +0200

    Create задача 1.js

---
+++ задача 1.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const items = [
+    { price: 40 },
+    { price: 120 },
+    { price: 505 },
+    { price: 350 }
+];
+
+for (const item of items){
+    console.log(`Price: ${item.price}`);
+}
