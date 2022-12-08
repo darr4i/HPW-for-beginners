e1d8d68dc8d09a7d451f1d87c3589eb4e3201b31
Daria Rabiichuk <115396338+darr4i@users.noreply.github.com>
2022-12-08 20:21:09 +0200

    Create задача 3.js

---
+++ задача 3.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const purchase = {
+    Electronics: [
+        { name: 'Laptop', price: 1500 },
+        { name: 'Keyboard', price: 100 },
+        { name: 'Monitor', price: 1800},
+    ],
+    Textile: [
+        { name: 'Bag', price: 50 },
+        { name: 'Scarf', price: 35},
+        { name: 'Glasses', price: 80}
+    ],
+    Furniture: [
+        { name: 'Table', price: 700},
+        { name: 'Mirror', price: 250},
+        { name: 'Sofa', price: 2300}
+    ]
+};
+console.log(purchase)
